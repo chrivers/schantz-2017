513ccfe08e505b983613115f9b6104f393ae18cf
Christian Iversen <ci@iversenit.dk>
2017-01-05 23:00:43 +0100

    * Minified 2017-04 further

--- 2017-04.java
+++ 2017-04.java
@@ -3,7 +3,7 @@ interface A {
         float X = 0;
         for (int Y = 0; Y < 70; Y++) {
             System.out.format("%d	%f\n", Y+2016, X *= 1.00847f);
-            X += (1e4f * (Y > 12 ? 2 : 1) * 0.89f) - (X > 2e5 ? 0 : 1e3);
+            X += 8900 * (Y > 12 ? 2 : 1) - (X > 2e5 ? 0 : 1e3);
         }
     }
 }
