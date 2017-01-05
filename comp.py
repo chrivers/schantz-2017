3e15f8ff20a561fb151c2d9c4889136fd2b7986b
Christian Iversen <ci@iversenit.dk>
2017-01-05 23:00:26 +0100

    + Added error level display

--- comp.py
+++ comp.py
@@ -19,6 +19,8 @@ if len(refs) != len(vals):
     print("Wrong number of values (%d actual vs %d expected)" % (len(vals), len(refs)))
 #    sys.exit(1)
 
+max_err = 0
+
 for i, ref in enumerate(refs):
     val = vals[i]
     diff = ref - val
@@ -26,4 +28,9 @@ for i, ref in enumerate(refs):
         desc = 0.0
     else:
         desc = diff / ref * 100.0
+    max_err = max(max_err, abs(desc))
     print("%d: %12.4f vs ref %12.4f: diff %12.4f (%8.4f%% error)" % (2016 + i, val, ref, diff, desc))
+
+max_err_level = 0.1
+print("max error: %.4f%%" % max_err)
+print("error level: %.2f%%" % (abs(max_err / max_err_level) * 100))
