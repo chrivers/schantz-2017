a51887eadd5d4dcff29e936bea6c8597ffe6858c
Christian Iversen <ci@iversenit.dk>
2017-01-05 22:10:27 +0100

    * Minified 2017-03 further

--- 2017-03.java
+++ 2017-03.java
@@ -1,12 +1,15 @@
 interface A {
     static void main(String[]a) {
         float L = 0f;
-        for (int Y = 2015; L < 1e6; Y++) {
+        float X = 0;
+        float Q = 0;
+        for (int Y = 2015; Q < 1e6; Y++) {
+            L = 1.00847f * X;
             float I, r, P;
             I = 1e4f * (Y > 2027 ? 2 : 1);
-            float X = L + I - (I * 0.1100f) - (L > 2e5 ? 0 : 1000);
+            X = L + I - (I * 0.1100f) - (L > 2e5 ? 0 : 1000);
             System.out.format("%d	%f\n", Y, L);
-            L = 1.00847f * X;
-       }
+            Q = L;
+    }
     }
 }
