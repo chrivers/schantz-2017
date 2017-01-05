4276b0f0633627648a177bf5bee84a2f04a274cb
Christian Iversen <ci@iversenit.dk>
2017-01-05 23:16:42 +0100

    * Iterated over 2017-04 further

--- 2017-04.java
+++ 2017-04.java
@@ -1,9 +1,10 @@
 interface A {
     static void main(String[]a) {
-        float X = 0;
-        for (int Y = 0; Y < 70; Y++) {
-            System.out.format("%d	%f\n", Y+2016, X *= 1.00847f);
-            X += 8900 * (Y > 12 ? 2 : 1) - (X > 2e5 ? 0 : 1e3);
+        float Y = 16, X = 0;
+        while (Y++ < 71) {
+            System.out.format("20%f	%f\n", Y, X *= 1.00847f);
+            X -= X > 2e5 ? 0 : 1e3;
+            X += Y > 29 ? 17800 : 8900;
         }
     }
 }
