f415092a8221afc44da4662c0847788981e1d83a
Christian Iversen <ci@iversenit.dk>
2017-01-06 20:04:20 +0100

    * Minified a little more

--- 2017-06.java
+++ 2017-06.java
@@ -1,9 +1,9 @@
 interface A {
     static void main(String[]a) {
         for (
-            long Y = -13, X = 0;
+            long Y = -13, X = 0, Z;
             Y++ < 42;
-            X += (Y > 0 ? Y > 6 ? 178 : 168 : 79) * 100
+            Z = Y > 0 ? Y > 6 ? 178 : 168 : 79, X += Z * 100
         ) System.out.format("20%d	%d\n", Y, X *= 1.00847);
     }
 }
