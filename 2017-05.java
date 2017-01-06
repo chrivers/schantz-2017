a72dd1a95429fcda7d39271f0c74f4d035ae5f2e
Christian Iversen <ci@iversenit.dk>
2017-01-06 18:43:02 +0100

    * Evil type tricks, and bad coding style

--- 2017-05.java
+++ 2017-05.java
@@ -1,9 +1,9 @@
 interface A {
     static void main(String[]a) {
         for (
-            float Y = 16, X = 0;
-            Y++ < 71;
-            X += Y > 29 ? Y > 35 ? 17800 : 16800 : 7900
-        ) System.out.format("20%f	%f\n", Y, X *= 1.00847);
+            long Y = -13, X = 0;
+            Y++ < 42;
+            X += (Y > 0 ? Y > 6 ? 178 : 168 : 79) * 100
+        ) System.out.format("20%d	%d\n", Y, X *= 1.00847);
     }
 }
