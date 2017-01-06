f3600e60674ceb238ceb34452f08e7ac3fbe1a0e
Christian Iversen <ci@iversenit.dk>
2017-01-06 20:03:50 +0100

    + Forked 05 to 06

---
+++ 2017-06.java
@@ -0,0 +1,9 @@
+interface A {
+    static void main(String[]a) {
+        for (
+            long Y = -13, X = 0;
+            Y++ < 42;
+            X += (Y > 0 ? Y > 6 ? 178 : 168 : 79) * 100
+        ) System.out.format("20%d	%d\n", Y, X *= 1.00847);
+    }
+}
