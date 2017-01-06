2810ca714d7116aa9e340be1c20a4de7fc8f6032
Christian Iversen <ci@iversenit.dk>
2017-01-07 00:56:24 +0100

    * One character less

--- 2017-06.java
+++ 2017-06.java
@@ -4,6 +4,6 @@ interface A {
             int Y = 16, X = 0;
             Y++ < 71;
             X += (Y > 29 ? Y > 35 ? 178 : 168 : 79) * 100
-        ) System.out.format("20%d	%d\n", Y, X *= 1.0085);
+        ) System.out.format("20%d	%d\n", Y, X += X/118);
     }
 }
