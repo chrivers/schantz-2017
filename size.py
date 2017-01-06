afefc9ec57a28d4c8ac802b0b5c69d46caba2e30
Christian Iversen <ci@iversenit.dk>
2017-01-07 00:48:49 +0100

    * Fixed estimate

--- size.py
+++ size.py
@@ -20,4 +20,4 @@ for filename in filenames:
     text = open(filename).read().strip()
     text2 = strip_whitespace(text)
 
-    print("%s: %6d total, %6d whitespace, %6d non-whitespace (%6d estimated real)" % (filename, len(text), len(text)-len(text2), len(text2), len(text2) + 6))
+    print("%s: %6d total, %6d whitespace, %6d non-whitespace (%6d estimated real)" % (filename, len(text), len(text)-len(text2), len(text2), len(text2) + 5))
