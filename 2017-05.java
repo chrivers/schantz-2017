4e713a695e3b52b0f06007ca87165d52b4fba1a2
Christian Iversen <ci@iversenit.dk>
2017-01-06 18:13:35 +0100

    * Began using cheap shots and obscure voodoo

--- 2017-05.java
+++ 2017-05.java
@@ -1,10 +1,9 @@
 interface A {
     static void main(String[]a) {
-        float Y = 16, X = 0;
-        while (Y++ < 71) {
-            System.out.format("20%f	%f\n", Y, X *= 1.00847);
-            X -= X > 2e5 ? 0 : 1e3;
-            X += Y > 29 ? 17800 : 8900;
-        }
+        for (
+            float Y = 16, X = 0;
+            Y++ < 71;
+            X -= X > 2e5 ? 0 : 1e3, X += Y > 29 ? 17800 : 8900
+        ) System.out.format("20%f	%f\n", Y, X *= 1.00847);
     }
 }
