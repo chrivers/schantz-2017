30ecd6aa3087786cf079e02741a9a3eef56629c2
Christian Iversen <ci@iversenit.dk>
2017-01-06 22:36:58 +0100

    * Fixed year output in 2017-05

--- 2017-05.java
+++ 2017-05.java
@@ -1,9 +1,9 @@
 interface A {
     static void main(String[]a) {
         for (
-            long Y = -13, X = 0;
-            Y++ < 42;
-            X += (Y > 0 ? Y > 6 ? 178 : 168 : 79) * 100
+            long Y = 16, X = 0;
+            Y++ < 71;
+            X += (Y > 29 ? Y > 35 ? 178 : 168 : 79) * 100
         ) System.out.format("20%d	%d\n", Y, X *= 1.00847);
     }
 }
