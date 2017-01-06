7b0f22b74afa3b8a3f302acea5970b24f16d7b73
Christian Iversen <ci@iversenit.dk>
2017-01-06 22:44:31 +0100

    * Shaved off 2 bytes more from 06

--- 2017-06.java
+++ 2017-06.java
@@ -1,9 +1,9 @@
 interface A {
     static void main(String[]a) {
         for (
-            long Y = -13, X = 0, Z;
-            Y++ < 42;
-            Z = Y > 0 ? Y > 6 ? 178 : 168 : 79, X += Z * 100
-        ) System.out.format("20%d	%d\n", Y, X *= 1.00847);
+            int Y = 16, X = 0;
+            Y++ < 71;
+            X += (Y > 29 ? Y > 35 ? 178 : 168 : 79) * 100
+        ) System.out.format("20%d	%d\n", Y, X *= 1.0085);
     }
 }
