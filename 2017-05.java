9abed7e8fefc238539d59a4a36294b62e6f77cb9
Christian Iversen <ci@iversenit.dk>
2017-01-06 18:20:26 +0100

    * Ternary expressions are evil ? only : sometimes

--- 2017-05.java
+++ 2017-05.java
@@ -3,7 +3,7 @@ interface A {
         for (
             float Y = 16, X = 0;
             Y++ < 71;
-            X -= X > 2e5 ? 0 : 1e3, X += Y > 29 ? 17800 : 8900
+            X += Y > 29 ? Y > 35 ? 17800 : 16800 : 7900
         ) System.out.format("20%f	%f\n", Y, X *= 1.00847);
     }
 }
