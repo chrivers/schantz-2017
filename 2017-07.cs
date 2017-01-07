7d6b277a04dc1cee8c10832b1f1924b78ed71221
Christian Iversen <ci@iversenit.dk>
2017-01-07 23:48:22 +0100

    * Fixed off-by-one error in year calculations

--- 2017-07.cs
+++ 2017-07.cs
@@ -3,9 +3,9 @@ class P
     static void Main()
     {
         for (
-            int Y = 16, X = 0;
-            Y++ < 71;
-            X += (Y > 29 ? Y > 35 ? 556 : 524 : 247) << 5
+            int Y = 15, X = 0;
+            Y++ < 70;
+            X += (Y > 28 ? Y > 34 ? 556 : 524 : 247) << 5
         ) System.Console.Write("20{0}	{1}\n", Y, X += X/118);
     }
 }
