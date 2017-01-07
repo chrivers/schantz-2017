59bcc5f2d4295377a0946f53dadad9d91d9a7803
Christian Iversen <ci@iversenit.dk>
2017-01-07 14:18:53 +0100

    * Converted minify.py to module

--- minify.py
+++ minify.py
@@ -2,17 +2,18 @@
 import sys
 import re
 
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
-else:
-    print("usage: %s <file>" % sys.argv[0])
-    sys.exit(1)
+def minify(text):
+    line = text.replace("\n", "")
+    line = re.sub("(?<=\w)[ ]+(?=\w)", "\x00", line)
+    line = line.replace(" ", "").replace("\x00", " ")
+    return line
 
-text = open(filename).read().strip()
-line = text.replace("\n", "")
-line = re.sub("(?<=\w)[ ]+(?=\w)", "!", line)
-#line = re.sub("(?<!\w)[ ]+(?!\w)", "", line)
-line = line.replace(" ", "").replace("!", " ")
-sys.stdout.write(line)
-
-#print("%s: %6d total, %6d whitespace, %6d non-whitespace (%6d estimated real)" % (filename, len(text), len(text)-len(text2), len(text2), len(text2) + 6))
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        text = open(filename).read().strip()
+        line = minify(text)
+        sys.stdout.write(line)
+    else:
+        print("usage: %s <file>" % sys.argv[0])
+        sys.exit(1)
