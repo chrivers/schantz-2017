a7d18b5c49579f5c4cc7d97c6e7326d9243880ce
Christian Iversen <ci@iversenit.dk>
2017-01-09 15:47:33 +0100

    * Robustified validator script for ref_val[0] == 0

--- comp.py
+++ comp.py
@@ -34,7 +34,7 @@ for i, ref in enumerate(zip(refs, comp)):
     diff = ref_val - cmp_val
     if ref_year != cmp_year:
         wrong_years += 1
-    if diff == 0.0:
+    if diff == 0.0 or ref_val == 0.0:
         desc = 0.0
     else:
         desc = diff / ref_val * 100.0
