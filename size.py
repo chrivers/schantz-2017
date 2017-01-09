05d174db230952fdf21599c868467962d4afd19e
Christian Iversen <ci@iversenit.dk>
2017-01-09 19:56:03 +0100

    * Handle binary files correctly

--- size.py
+++ size.py
@@ -18,10 +18,12 @@ else:
     sys.exit(1)
 
 for filename in filenames:
-    text = open(filename,errors='surrogateescape').read().strip()
-    text2 = strip_whitespace(text)
+    text = open(filename,errors='surrogateescape').read()
     if filename.endswith(".cs") or filename.endswith(".java"):
+        text = text.strip()
+        text2 = strip_whitespace(text)
         mindesc = "%6d characters (minified)" % len(minify.minify(text))
     else:
+        text2 = text
         mindesc = "%6d bytes (binary)" % len(text)
     print("%-15s: %6d total, %6d whitespace, %6d non-whitespace: %s" % (filename, len(text), len(text)-len(text2), len(text2), mindesc))
