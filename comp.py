b157963bd1204accc38e59d40f58216fb5c30c6a
Christian Iversen <ci@iversenit.dk>
2017-01-09 01:54:26 +0100

    + Implemented crude bar graph for error level

--- comp.py
+++ comp.py
@@ -39,7 +39,14 @@ for i, ref in enumerate(zip(refs, comp)):
     else:
         desc = diff / ref_val * 100.0
     max_err = max(max_err, abs(desc))
-    print("%d: %12.4f vs ref %12.4f: diff %12.4f (%s error)" % (2016 + i, cmp_val, ref_val, diff, color_result("%8.4f%%" % -desc, abs(desc) < max_err_level)))
+    if abs(desc) < max_err_level:
+        num_good = abs(desc) / max_err_level * 10
+        num_bad  = 0
+    else:
+        num_good = 10
+        num_bad = (abs(desc) - max_err_level) / max_err_level * 10
+    bar = color_result("*" * int(num_good), True) + color_result("*" * int(num_bad), False)
+    print("%d: %12.4f vs ref %12.4f: diff %12.4f (%s error)%s" % (2016 + i, cmp_val, ref_val, diff, color_result("%8.4f%%" % -desc, abs(desc) < max_err_level), bar))
 
 print("-"*80)
 print("Number of incorrect years [%d] [%s]" % (wrong_years, check(wrong_years == 0)))
