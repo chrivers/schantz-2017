c7c3df12da7e1e3dafb21b7d571f64a31935b89e
Christian Iversen <ci@iversenit.dk>
2017-01-09 20:00:50 +0100

    * Used techniques from the C# version to remove 2 bytes

--- 2017-06.java
+++ 2017-06.java
@@ -1,9 +1,9 @@
 interface A {
     static void main(String[]a) {
         for (
-            int Y = 16, X = 0;
-            Y++ < 71;
-            X += (Y > 29 ? Y > 35 ? 278 : 262 : 123) << 6
-        ) System.out.format("20%d	%d\n", Y, X += X/118);
+            int Y = 15, X = 0;
+            Y++ < 70;
+            X += Y > 28 ? 524 + Y / 35 * 32 << 5 : 7904
+        ) System.out.format("20%d	%d\n", Y, X += X / 118);
     }
 }
