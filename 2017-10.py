d5c5c31130125f4d0f8b12ea9f8b13894280469a
Christian Iversen <ci@iversenit.dk>
2017-01-20 13:45:32 +0100

    * Experiments with check10, to attempt a smaller gs2 solution

--- 2017-10.py
+++ 2017-10.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
+
+import sys
+def log(x, *args):
+    sys.stderr.write(x % args)
+    sys.stderr.write("\n")
+
 X = 0
 for Y in range(16, 71):
     print "20%d\t%s" % (Y, X)
-    X += ((1000 * ((Y > 34)+1)) + 7900) * (Y > 28)
-    X += 7900
+    ycmp1 = int(Y > 34)
+    ycmp2 = int(Y > 28)
+    A = 1000 * (ycmp2 + ycmp1)
+    log("%4d: ycmp1=%d ycmp2=%d, A=%d", Y + 2000, ycmp1, ycmp2, A)
+    X += A + 7900 * (ycmp2+1)
     X += X / 118
-
