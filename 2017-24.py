7b46df4bc5bda6fc506a1c96ff184699ad1db0d4
Christian Iversen <ci@iversenit.dk>
2017-01-30 12:04:58 +0100

    * Compacted v24

--- 2017-24.py
+++ 2017-24.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 #encoding=utf-8
 def foo(q, y):
-    nx = q[1] + ord(u"Ἕ䈯䘐"[(y>12)+(y>18)]) + q[1]/118
-    return (q[0] + "20%d	%s\n" % (16+y,q[1])), nx
+    return (q[0] + "20%d	%s\n" % (16+y,q[1])), q[1] + ord(u"Ἕ䈯䘐"[(y>12)+(y>18)]) + q[1]/118
 
 print reduce(
     foo,
