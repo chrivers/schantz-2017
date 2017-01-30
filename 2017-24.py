69c02559f210b7724ecea2d1c5b91d8750e6abb2
Christian Iversen <ci@iversenit.dk>
2017-01-30 12:04:38 +0100

    * Experiments with python version using reduce()

--- 2017-24.py
+++ 2017-24.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 #encoding=utf-8
-q = []
-reduce(
-    lambda x, y: q.append("20%d	%s" % (16+y,x)) or x + ord(u"Ἕ䈯䘐"[(y>12)+(y>18)]) + x/118,
+def foo(q, y):
+    nx = q[1] + ord(u"Ἕ䈯䘐"[(y>12)+(y>18)]) + q[1]/118
+    return (q[0] + "20%d	%s\n" % (16+y,q[1])), nx
+
+print reduce(
+    foo,
     range(55),
-    0
-)
-print "\n".join(q)
+    ("", 0)
+)[0],
