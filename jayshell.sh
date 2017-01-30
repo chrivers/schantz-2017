34e77b170b3819042159c9588dcd65f01d957c41
Christian Iversen <ci@iversenit.dk>
2017-01-30 11:20:53 +0100

    + Implemented "jayshell" for simulating dead-slow jshell program

---
+++ jayshell.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## Since "jshell" takes a ludicrous 9-18 seconds to start up, we can
+## use this script to get the same computation by manually wrapping
+## the source in a java interface, and running javac. This gives the
+## same semantics, but about 98% faster.
+
+NAME=javatmp_$$
+printf "interface $NAME { static void printf(String format, Object... args) { System.out.printf(format, args); }; static void main(String[]a) { %s } }" "$(<$1)" > $NAME.java
+
+javac $NAME.java
+java $NAME
+
+rm -f $NAME.java $NAME.class
