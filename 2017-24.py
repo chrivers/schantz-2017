cecc1c209a29cf578e63c7893c62e4de299c1ed4
Christian Iversen <ci@iversenit.dk>
2017-01-30 12:05:41 +0100

    * Replaced def foo() with lambda

--- 2017-24.py
+++ 2017-24.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
 #encoding=utf-8
-def foo(q, y):
-    return (q[0] + "20%d	%s\n" % (16+y,q[1])), q[1] + ord(u"Ἕ䈯䘐"[(y>12)+(y>18)]) + q[1]/118
 
 print reduce(
-    foo,
+    lambda q,y: (q[0] + "20%d	%s\n" % (16+y,q[1]), q[1] + ord(u"Ἕ䈯䘐"[(y>12)+(y>18)]) + q[1]/118),
     range(55),
     ("", 0)
 )[0],
