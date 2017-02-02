9a036642e51c72ca7da044b4cc5423bc85943593
Christian Iversen <ci@iversenit.dk>
2017-02-02 22:14:31 +0100

    * Manually minified python solution. The new pseudo-functional approach almost gets rid of whitespace, but sadly the combinator functions are too verbose to be better overall

--- 2017-24.py
+++ 2017-24.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python
 #encoding=utf-8
-
-print reduce(
-    lambda q,y: (q[0] + "20%d	%s\n" % (16+y,q[1]), q[1] + ord(u"Ἕ䈯䘐"[(y>12)+(y>18)]) + q[1]/118),
-    range(55),
-    ("", 0)
-)[0],
+Z=0
+print reduce(lambda q,y:(q[0]+"20%d	%s\n"%(16+y,q[1]),q[1]+ord(u"Ἕ䈯䘐"[(y>12)+(y>18)])+q[1]/118),range(55),("",0))[0],
